e0261af009141102f141301dd0a36c86ac84af7c
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2023-09-11 20:50:42 +0100

    changed container port :zap:

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,7 +6,7 @@ max_requests_jitter = 50
 
 log_file = "-"
 
-bind = "0.0.0.0:8080"
+bind = "0.0.0.0:3200"
 
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = (multiprocessing.cpu_count() * 2) + 1
